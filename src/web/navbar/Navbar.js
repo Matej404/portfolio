b1fc49240f41b24edcaca16e0404bd6db51dfdc3
Matej404 <matejjurin278@gmail.com>
2022-01-25 17:11:26 +0000

    navbar-responsive

--- src/web/navbar/Navbar.js
+++ src/web/navbar/Navbar.js
@@ -1,10 +1,34 @@
 import "./navbar.css";
 import { Link } from "react-scroll";
+import { FaBars } from "react-icons/fa";
+import { useState, useEffect } from "react";
+
 
 export default function Navbar() {
+    const [toggleMenu, setToggleMenu] = useState(false);
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+    
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu);
+    }
+
+    useEffect(() => {
+        const changeWidth = () => {
+            setScreenWidth(window.innerWidth);
+        }
+        window.addEventListener('resize', changeWidth);
+
+        return () => {
+            window.removeEventListener('resize', changeWidth);
+        }
+    }, [])
+
     return(
         <div className="navbar">
-            <div className="nav-container">
+            <div className="mobile-icon" onClick={toggleNav}>
+                    <FaBars />
+                </div>
+            {(toggleMenu || screenWidth > 768) && (<div className="nav-container">
                 <ul className="nav-menu">
                     <li className="nav-link">
                         <Link className="nav-link" to="home" smooth={true} duration={1000} >
@@ -27,10 +51,11 @@ export default function Navbar() {
                         </Link>
                     </li>
                 </ul>
+                
+            </div>)}
                 <div className="nav-btn">
                     <a className="nav-btn-link" href="https://github.com/Matej404" target="_blank">GitHub</a>
                 </div>
-            </div>
         </div>
     )
 }
