9f4b8a3bc41dd36fe1a02fcafe4f22c72d0dc770
Matej404 <matejjurin278@gmail.com>
2022-02-07 01:27:24 +0000

    contact-section functionality

--- src/web/contact/Contact.js
+++ src/web/contact/Contact.js
@@ -1,17 +1,43 @@
 import "./contact.css";
 import { BsTelephoneFill } from "react-icons/bs";
-import { AiOutlineMail } from "react-icons/ai";
+import { AiOutlineMail, AiOutlineClose } from "react-icons/ai";
 import { GoLocation } from "react-icons/go";
 import  Fade  from "react-reveal/Fade";
+import emailjs from "emailjs-com";
+import React, { useState } from "react";
 
 export default function Contact() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+    const [popUpMessage, setPopUpMessage] = useState(false);
+    const isTextareaDisabled = message.length === 0;
+
+    
+    const handlePopUpMessage = () => {
+        setPopUpMessage(!popUpMessage);
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        setName("");
+        setEmail("");
+        setMessage("");
+        handlePopUpMessage();
+        emailjs.sendForm("service_844y6qe", "template_p7u54e4", e.target, "user_nP5ZPxQvHxvU4nh8EF4Hz").then(res => {
+            console.log(res);
+        }).catch(err=> console.log(err));
     }
+
 
     return(
         <div className="contact">
+            {popUpMessage && (
+                <div className="pop-up-message">
+                    <AiOutlineClose className="close-message" onClick={handlePopUpMessage} />
+                    <p>Your message has been successfully submitted!</p>
+                </div>)}
+           
             <Fade clear>
                 <h1 className="contact-hadline">Contact</h1>
             </Fade>
@@ -41,17 +67,17 @@ export default function Contact() {
                         <form onSubmit={handleSubmit}>
                             <div className="form-grup">
                                 <label for="name">Your Name</label>
-                                <input type="text" id="name" name="name" placeholder="Enter your name" />
+                                <input type="text" id="name" name="name" placeholder="Enter your name" value={name} onChange={(e) => setName(e.target.value)} />
                             </div>
                             <div className="form-grup">
                                 <label>Your E-mail</label>
-                                <input type="email" id="email" name="email" placeholder="Enter your E-mail" />
+                                <input type="email" id="email" name="email" placeholder="Enter your E-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
                             </div>
                             <div className="form-grup">
                                 <label for="message">Your Message</label>
-                                <textarea id="message" name="message" rows="6" cols="80" placeholder="Enter Your message" />
+                                <textarea id="message" name="message" rows="6" cols="80" placeholder="Enter Your message" value={message} onChange={(e) => setMessage(e.target.value)} />
                             </div>
-                            <button type="submit" className="btn-submit">Send</button>
+                            <button type="submit" className="btn-submit" disabled={isTextareaDisabled}>Submit</button>
                         </form>
                     </div>
                 </div>
