e7343ba3644696f574ebbdd1de011b90c9cbe272
Matej404 <matejjurin278@gmail.com>
2022-04-24 11:19:04 +0100

    changes

--- src/web/projects/projectArry.js
+++ src/web/projects/projectArry.js
@@ -4,15 +4,6 @@ import { CgGym } from 'react-icons/cg';
 
 export const projects = [
     {
-        id: "spotify",
-        icon: <BsSpotify className='project-icon' />,
-        name: "Spotify",
-        ghPages: "https://matej404.github.io/Spotify/",
-        ghCode: "https://github.com/Matej404/Spotify",
-        techStack:"React, React-router-dom, Node.js",
-        about: "This website is still in process. I am copying the look and functionality of the actual Spotify website here but, I am using my own code."
-    },
-    {
         id: "gym",
         icon: <CgGym className='project-icon' />,
         name: "Gym Website",
@@ -20,24 +11,6 @@ export const projects = [
         ghCode: "https://github.com/Matej404/TimGym",
         techStack:"React, React-router-dom, Node.js",
         about: "I made a gym website here. I didn't have enough details and pictures about the gym, but I used everything I had."
-    },
-    {
-        id: "chat",
-        icon: <BsFillChatLeftDotsFill className='project-icon' />,
-        name: "Chat App",
-        ghPages: "https://matej404.github.io/chat/",
-        ghCode: "https://github.com/Matej404/chat",
-        techStack:"React, Node.js",
-        about: "When app is started it generates You a random name and random color. To have multiple users online You need to open the app in multiple tabs. Scaledrone Chat Service used. Scaledrone provides features like realtime messaging, message history and user presence so you can focus on the business logic."
-    },
-    {
-        id: "quote",
-        icon: <BsBlockquoteLeft className='project-icon' />,
-        name: "Quote Machine App",
-        ghPages: "https://matej404.github.io/Quote-machine-react/",
-        ghCode: "https://github.com/Matej404/Quote-machine-react",
-        techStack:"React, Node.js",
-        about: "This was suggestion from freeCodeCamp. I used my own code."
     },
     {
         id: "slider",
@@ -49,6 +22,24 @@ export const projects = [
         about: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
     },
     {
+        id: "quote",
+        icon: <BsBlockquoteLeft className='project-icon' />,
+        name: "Quote Machine App",
+        ghPages: "https://matej404.github.io/Quote-machine-react/",
+        ghCode: "https://github.com/Matej404/Quote-machine-react",
+        techStack:"React, Node.js",
+        about: "This was suggestion from freeCodeCamp. I used my own code."
+    },
+    {
+        id: "chat",
+        icon: <BsFillChatLeftDotsFill className='project-icon' />,
+        name: "Chat App",
+        ghPages: "https://matej404.github.io/chat/",
+        ghCode: "https://github.com/Matej404/chat",
+        techStack:"React, Node.js",
+        about: "When app is started it generates You a random name and random color. To have multiple users online You need to open the app in multiple tabs. Scaledrone Chat Service used. Scaledrone provides features like realtime messaging, message history and user presence so you can focus on the business logic."
+    },
+    {
         id: "generator",
         icon: <MdPassword className='project-icon' />,
         name: "Pasword Generator",
@@ -56,5 +47,14 @@ export const projects = [
         ghCode: "https://github.com/Matej404/password-generator",
         techStack:"JavaScript, sCSS, HTML5",
         about: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },
+    {
+        id: "spotify",
+        icon: <BsSpotify className='project-icon' />,
+        name: "Spotify",
+        ghPages: "https://matej404.github.io/Spotify/",
+        ghCode: "https://github.com/Matej404/Spotify",
+        techStack:"React, React-router-dom, Node.js",
+        about: "This website is still in process. I am copying the look and functionality of the actual Spotify website here but, I am using my own code."
     }
 ]
