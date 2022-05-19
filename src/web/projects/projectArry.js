ff86ca0680eb264d929af14c0ed32cdc1c001c7a
Matej404 <matejjurin278@gmail.com>
2022-05-19 23:51:19 +0100

    add-budget-app

--- src/web/projects/projectArry.js
+++ src/web/projects/projectArry.js
@@ -1,6 +1,9 @@
-import {BsFillChatLeftDotsFill, BsBlockquoteLeft, BsSliders, BsSpotify} from 'react-icons/bs';
-import {MdPassword} from 'react-icons/md';
+import "./projects.css";
+import { BsFillChatLeftDotsFill, BsBlockquoteLeft, BsSliders, BsSpotify } from 'react-icons/bs';
+import { MdPassword } from 'react-icons/md';
 import { CgGym } from 'react-icons/cg';
+import { GrMoney } from 'react-icons/gr';
+import { RiCoinFill } from 'react-icons/ri';
 
 export const projects = [
     {
@@ -13,12 +16,21 @@ export const projects = [
         about: "I made a gym website here. I didn't have enough details and pictures about the gym, but I used everything I had."
     },
     {
+        id: "budget",
+        icon: <RiCoinFill className='project-icon' />,
+        name: "Budget App",
+        ghPages: "https://matej404.github.io/budget-app/",
+        ghCode: "https://github.com/Matej404/budget-app",
+        techStack:"React, Bootstrap, Node.js",
+        about: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },
+    {
         id: "slider",
         icon: <BsSliders className='project-icon' />,
         name: "Image Slider App",
         ghPages: "https://matej404.github.io/Img-slider/",
         ghCode: "https://github.com/Matej404/Img-slider",
-        techStack:"JavaScript, CSS, HTML5",
+        techStack:"React, Node.js",
         about: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
     },
     {
@@ -36,7 +48,7 @@ export const projects = [
         name: "Chat App",
         ghPages: "https://matej404.github.io/chat/",
         ghCode: "https://github.com/Matej404/chat",
-        techStack:"React, Node.js",
+        techStack:"React, scaledron, Node.js",
         about: "When app is started it generates You a random name and random color. To have multiple users online You need to open the app in multiple tabs. Scaledrone Chat Service used. Scaledrone provides features like realtime messaging, message history and user presence so you can focus on the business logic."
     },
     {
